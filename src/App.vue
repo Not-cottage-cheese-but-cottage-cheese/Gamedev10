f176fb456c08f211a1df3caa170c33653dec2a7b
Aleksey Semenov <43957407+borgishmorg@users.noreply.github.com>
2022-04-24 12:38:24 +0200

    feat: add game component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GameComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameComponent from './components/GameComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameComponent
   }
 }
 </script>
